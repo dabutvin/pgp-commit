cad3c2a9cc572a2b4028b3c8682ab64577f5a5fd
Dan Butvinik <dabutvin@microsoft.com>
2018-12-13 10:02:46 -0800

    sign a message

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 const git = require('nodegit')
 const openpgp = require('openpgp')
+
+async function sign(message, privateKey, passphrase) {
+  const privateKeyResult = (await openpgp.key.readArmored(privateKey)).keys[0]
+  if (!privateKeyResult) throw new Error('unable to read private key')
+  await privateKeyResult.decrypt(passphrase)
+  const options = {
+    message: openpgp.cleartext.fromText(message),
+    privateKeys: [privateKeyResult],
+    detached: true
+  }
+  const signed = await openpgp.sign(options)
+  const signature = signed.signature.replace(/\r\n/g, '\n')
+  return signature
+}
+
+module.exports = sign
