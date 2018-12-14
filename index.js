85738f31838a62991281a1242e97c91af7789eb7
Dan Butvinik <dabutvin@microsoft.com>
2018-12-14 09:37:04 -0800

    sign commit and update HEAD

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs')
 
 async function sign(message, privateKey, passphrase) {
   const repo = await git.Clone(
-    'https://github.com/dabutvin/test',
+    'https://github.com/dabutvin/deepstream',
     '/private/tmp/test'
   )
 
@@ -13,31 +13,37 @@ async function sign(message, privateKey, passphrase) {
   const index = await repo.refreshIndex()
   await index.addAll()
   await index.write()
-  const oid = await index.writeTree()
+  const oid = await index.writeTreeTo(repo)
+  const branch = await repo.getBranch('HEAD')
   const head = await git.Reference.nameToId(repo, 'HEAD')
   const parent = await repo.getCommit(head)
   const author = git.Signature.now('test', 'test@test.com')
   const committer = git.Signature.now('test', 'test@test.com')
-  const commitId = await repo.createCommit(
-    'HEAD',
+  const commit = await repo.createCommitWithSignature(
     author,
     committer,
-    'message',
+    message,
     oid,
-    [parent]
+    [parent],
+    'gpgsig',
+    onSignature
   )
 
-  const privateKeyResult = (await openpgp.key.readArmored(privateKey)).keys[0]
-  if (!privateKeyResult) throw new Error('unable to read private key')
-  await privateKeyResult.decrypt(passphrase)
-  const options = {
-    message: openpgp.cleartext.fromText(message),
-    privateKeys: [privateKeyResult],
-    detached: true
-  }
-  const signed = await openpgp.sign(options)
-  const signature = signed.signature.replace(/\r\n/g, '\n')
-  return signature
-}
+  await branch.setTarget(commit.toString(), 'updating head')
+  return commit.toString()
 
+  async function onSignature(tosign) {
+    const privateKeyResult = (await openpgp.key.readArmored(privateKey)).keys[0]
+    if (!privateKeyResult) throw new Error('unable to read private key')
+    await privateKeyResult.decrypt(passphrase)
+    const options = {
+      message: openpgp.cleartext.fromText(tosign),
+      privateKeys: [privateKeyResult],
+      detached: true
+    }
+    const signed = await openpgp.sign(options)
+    const signature = signed.signature.replace(/\r\n/g, '\n')
+    return signature
+  }
+}
 module.exports = sign
